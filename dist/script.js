bfab6ce76c46d14f19b800e667d9f1bff4ae84c3
NirajKumaroy <nirajbgs0@gmail.com>
2024-07-17 21:22:00 +0530

    first commit

---
+++ dist/script.js
@@ -0,0 +1,4 @@
+let typed = new Typed('#element', {
+    strings: ['Mobile App Developer', 'Graphic Designer', 'UI/UX Designer', 'Video Editor','C++','java', 'Pythan', 'FullStack web Developer👋'],
+    typeSpeed: 30,
+});
